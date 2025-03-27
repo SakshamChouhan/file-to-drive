7dc4ec6442d624ed4d3f3b9e57b63bc3310315a4
Saksham Chouhan <raisaksham426@example.com>
2025-03-27 21:20:25 +0530

    Initial commit is done

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import themePlugin from "@replit/vite-plugin-shadcn-theme-json";
 import path, { dirname } from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 import { fileURLToPath } from "url";
+import { cartographer } from "@replit/vite-plugin-cartographer";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -15,11 +16,7 @@ export default defineConfig({
     themePlugin(),
     ...(process.env.NODE_ENV !== "production" &&
     process.env.REPL_ID !== undefined
-      ? [
-          await import("@replit/vite-plugin-cartographer").then((m) =>
-            m.cartographer(),
-          ),
-        ]
+      ? [cartographer()]
       : []),
   ],
   resolve: {
